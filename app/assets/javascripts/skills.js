20a31d7f73461e7146455ea0b1041442fa7a69e1
markjflores <flores.markj@gmail.com>
2012-08-28 17:02:08 -0700

    Included basic formatting for base skill list and dataTable.

--- app/assets/javascripts/skills.js
+++ app/assets/javascripts/skills.js
@@ -9,3 +9,18 @@ function show_user_email_form(elem) {
   user_id = elem.parents('tr').find('td:last .user_id').val();
   alert("Should show user email form here for " + elem.parents('tr').find('td:first').html() + ":\nWith user_id " + user_id);
 }
+
+function user_table_formatting() {
+	$("#users").dataTable({"bAutoWidth": false,
+	  "bFilter": false,
+	  "bInfo": false,
+	  "bSort": false,
+	  "bPaginate": false,
+	  "sDom": "flrtip",
+	  "aoColumns": [ { sClass: "tbl_column_align_left" },
+	  { sClass: "tbl_column_align_left" },
+	  { sClass: "tbl_column_align_left" },
+	  { sClass: "tbl_column_align_right" },
+	  { sClass: "tbl_column_align_right" },
+	  { sClass: "tbl_column_align_center" }]}); 
+}
