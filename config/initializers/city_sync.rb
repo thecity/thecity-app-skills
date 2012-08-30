35c5e21f65d6e755ee59f64d63f1fc4dafb16b65
weshays <weshays@gbdev.com>
2012-08-30 14:04:08 -0700

    Updated settings to work with heroku.

---
+++ config/initializers/city_sync.rb
@@ -0,0 +1,13 @@
+if %w(development test).include?(Rails.env)
+  config_file = Rails.root.join('config', 'city_keys.yml')
+
+  if File.exists?(config_file)
+    yaml_data = YAML::load(File.open(config_file))
+    TCA_KEY = yaml_data[Rails.env]['tca_key']
+    TCA_TOKEN = yaml_data[Rails.env]['tca_token']
+  end
+
+else
+  TCA_KEY = ENV['TCA_KEY']
+  TCA_TOKEN = ENV['TCA_TOKEN']
+end
--- config/initializers/the_city_admin.rb
+++
@@ -1,3 +0,0 @@
-TCA_ENV = 'development'
-TCA_KEY = 'cf2903151e3213e66fd8080c7d8b65b1d6ccdd31'
-TCA_TOKEN = '5c88b32edda7653c'
